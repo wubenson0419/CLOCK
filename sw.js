eafffe70279adcfdee063ef3e6239dfd64f49fde
wubenson0419 <126497777+wubenson0419@users.noreply.github.com>
2025-06-05 22:34:33 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
-const CACHE_NAME = "clock-cache-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "https://fonts.googleapis.com/css2?family=Major+Mono+Display&display=swap",
-  "https://raw.githubusercontent.com/wubenson0419/ClockWebsite_TWtime/refs/heads/main/clockwebsite_twtime_icon.png",
-  "https://github.com/wubenson0419/ClockWebsite_TWtime/blob/main/iPhoneClock_04_19.png?raw=true"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('clock-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json'
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
   );
 });
